09f327d31b3172aa514983742cfc16515353848d
Ayush Jain <ayush.ayushjain12@gmail.com>
2020-04-19 02:44:44 +0530

    Updates

---
+++ precache-manifest.fe92826ccc6aa6f8b649ed4be5ce8e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1d57daf3b44edffad8727bf31aaf38",
+    "url": "/FIrstReact/index.html"
+  },
+  {
+    "revision": "dbed2c426a0cdd98202e",
+    "url": "/FIrstReact/static/css/main.163aa703.chunk.css"
+  },
+  {
+    "revision": "eff3142aacb744b4796e",
+    "url": "/FIrstReact/static/js/2.93748e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FIrstReact/static/js/2.93748e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbed2c426a0cdd98202e",
+    "url": "/FIrstReact/static/js/main.5281d4ab.chunk.js"
+  },
+  {
+    "revision": "2e2d13534dea4640b21d",
+    "url": "/FIrstReact/static/js/runtime-main.aa74ab56.js"
+  }
+]);
